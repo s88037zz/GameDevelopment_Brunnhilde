b44272c9c96ae0b9f6f2e141f1488594bc14b531
s88037zz <s88037zz@gmail.com>
2022-04-19 19:42:00 +0800

    建立elf actor
    建立elf 的物件

---
+++ Source/Brunnhilde/Private/Elf.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Elf.h"
+
+// Sets default values
+AElf::AElf()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AElf::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AElf::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+

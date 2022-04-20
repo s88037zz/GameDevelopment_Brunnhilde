8cbdb1d6938fd57241913a0f8fc6cc12139cf9ce
s88037zz <s88037zz@gmail.com>
2022-04-20 21:17:49 +0800

    [Add] 新增DroppingTresureAbility
    讓角色可以有掉落道具的能力

---
+++ Source/Brunnhilde/Public/Characters/Actions/DropingTruesureAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/Actions/Ability2.h"
+#include "DropingTruesureAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BRUNNHILDE_API UDropingTruesureAbility : public UAbility2
+{
+	GENERATED_BODY()
+	
+};
